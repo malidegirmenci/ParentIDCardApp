d504ad0c00aa03f9dfc489420ec634b8ab50645b
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-08-24 21:35:37 +0300

    sets page_setup settings and font, some renamed vars

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import openpyxl
 from openpyxl.drawing.image import Image
 from openpyxl.worksheet.page import PageMargins
+from openpyxl.styles import Font
 
 dataFile = 'ParentInfoList.xlsx'
 wb = openpyxl.load_workbook(dataFile)
 ws_data = wb.active
 ws_id_cards = wb.create_sheet(title="Kimlik Kartları")
 
-ws_id_cards.page_margins = PageMargins(left=0.5, right=0.5, top=0.75, bottom=0.75)
+ws_id_cards.page_setup.orientation = ws_id_cards.ORIENTATION_PORTRAIT
+ws_id_cards.page_setup.paperSize = ws_id_cards.PAPERSIZE_A4
+ws_id_cards.page_margins = PageMargins(left=0.5, right=0.5, top=0.5, bottom=0.75)
+arial_font = Font(name='Arial', size=11)
 
 ws_id_cards.row_dimensions[1].height = 10
 ws_id_cards.column_dimensions['A'].width = 2
@@ -35,7 +39,8 @@ def formats_text(text):
     return formatted_text
 
 for index, row in enumerate(ws_data.iter_rows(min_row=2, values_only=True)):
-    parentName, studentName, levelOfStudent, classOfStudent = row
+
+    parent_name, student_name, level_of_student, class_of_student = row
 
     col_offset = (index % 2) * 4  # Eğer index çiftse sola yerleşir, tekse sağa yerleşir.
     current_col = start_col + col_offset
@@ -50,26 +55,35 @@ for index, row in enumerate(ws_data.iter_rows(min_row=2, values_only=True)):
         print(f"{image_path_top} could not be found.")
 
     ws_id_cards.merge_cells(f'{chr(65 + current_col)}{start_row + 1}:{chr(65 + current_col + 2)}{start_row + 4}')
-    ws_id_cards[f'{chr(65 + current_col)}{start_row + 6}'] = f'Veli Ad Soyad'
-    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 6}'] = f': {parentName}'
-    ws_id_cards[f'{chr(65 + current_col)}{start_row + 7}'] = f'Öğrenci Ad Soyad'
-    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 7}'] = f': {studentName}'
-    ws_id_cards[f'{chr(65 + current_col)}{start_row + 8}'] = 'Kademe'
-    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 8}'] = f': {levelOfStudent}'
-    ws_id_cards[f'{chr(65 + current_col)}{start_row + 9}'] = 'Sınıf'
-    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 9}'] = f': {classOfStudent}'
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 5}'] = f'Veli Ad Soyad'
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 5}'] = f': {formats_text(parent_name)}'
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 6}'] = f'Öğrenci Ad Soyad'
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 6}'] = f': {formats_text(student_name)}'
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 7}'] = 'Kademe'
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 7}'] = f': {level_of_student}'
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 8}'] = 'Sınıf'
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 8}'] = f': {class_of_student}'
+
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 5}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 5}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 6}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 6}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 7}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 7}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col)}{start_row + 8}'].font = arial_font
+    ws_id_cards[f'{chr(65 + current_col + 1)}{start_row + 8}'].font = arial_font
 
     try:
         image_path_bottom = 'img/BKKimlikAlt.png'
         img_bottom = Image(image_path_bottom)
         img_bottom.width = 310
         img_bottom.height = 10
-        ws_id_cards.add_image(img_bottom, f'{chr(65 + current_col)}{start_row + 11}')
+        ws_id_cards.add_image(img_bottom, f'{chr(65 + current_col)}{start_row + 10}')
     except FileNotFoundError:
         print(f"{image_path_bottom} could not be found.")
 
     if index % 2 == 1:  # İki kimlik yan yana yerleştirildikten sonra bir alt satıra geçilir.
-        start_row += 15
+        start_row += 12
 
 wb.save('ParentIDCardList.xlsx')
 print('Created ID Cards')
